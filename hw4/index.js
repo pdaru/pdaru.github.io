0475455a10bb978974e8520f562a063684f0ad0e
daru.pankil05@gmail.com <pdaru>
2019-04-13 21:34:33 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -49,13 +49,11 @@ function doPredict(predict) {
   const result = predict(textField.value);
   score_string = "Class scores: ";
   for (var x in result.score) {
-    //score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
-    score_string += x + " ->  " + result.score[x].toFixed(4) + ", "
+    score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
   }
   //console.log(score_string);
   status(
-      #score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
-      score_string + ' elapsed: ' + result.elapsed.toFixed(4) + ' ms)');
+      score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
 }
 
 function prepUI(predict) {
@@ -138,7 +136,7 @@ class Classifier {
     status('Running inference');
     const beginMs = performance.now();
     const predictOut = this.model.predict(input);
-    console.log(predictOut.dataSync());
+    //console.log(predictOut.dataSync());
     const score = predictOut.dataSync();//[0];
     predictOut.dispose();
     const endMs = performance.now();
